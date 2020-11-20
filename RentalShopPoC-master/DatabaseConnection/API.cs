d0db0324588cd4834035ac332642c3c8df159bb4
Johannes Anttonen <johannesAnttonen@users.noreply.github.com>
2020-11-20 18:18:16 +0100

    fixat

---
+++ RentalShopPoC-master/DatabaseConnection/API.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseConnection
+{
+    public static class API
+    {
+        static Context ctx;
+
+        static API()
+        {
+            ctx = new Context();
+        }
+        public static List<Movie> GetMovieSlice(int a, int b)
+        {
+            return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
+        }
+        public static Customer GetCustomerByName(string name)
+        {
+            return ctx.Customers.FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
+        }
+        public static bool RegisterSale(Customer customer, Movie movie)
+        {
+            try
+            {
+                ctx.Add(new Rental() { Date = DateTime.Now, Customer = customer, Movie = movie });
+                return ctx.SaveChanges() == 1;
+            }
+            catch(DbUpdateException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                System.Diagnostics.Debug.WriteLine(e.InnerException.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ RentalShopPoC-master/Store/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DatabaseConnection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store
+{
+    // Håller koll på den data som programmet måste komma ihåg mellan vyer/fönster förändringar
+    static class State
+    {
+        public static Customer User { get; set; }
+        public static List<Movie> Movies { get; set; }
+        public static Movie Pick { get; set; }
+    }
+}
