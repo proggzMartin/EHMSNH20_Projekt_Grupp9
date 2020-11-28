d7c639958970bfdf60b025ec060adf68bbdd5547
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2020-11-28 10:56:04 +0100

    Addat en Notes-fil med anteckningar. Addat Automapper-nuget.

--- RentalShopPoC-master/Store/LoginWindow.xaml.cs
+++ RentalShopPoC-master/Store/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,17 +11,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using DatabaseConnection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Store
 {
+
+    
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
     public partial class LoginWindow : Window
     {
+        private class CustomerDto
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        Context _context;
+
+        List<CustomerDto> namesAndIds = new List<CustomerDto>();
+
         public LoginWindow()
         {
             InitializeComponent();
+            _context = new Context();
+
+            //namesAndIds = _context.Customers.Select(x => x.Name && x.Id)
         }
 
         private void LogIn_Click(object sender, RoutedEventArgs e)
