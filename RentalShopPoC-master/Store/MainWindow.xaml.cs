6a27e193bb46ff4c26e2950723d2c81f8c688972
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2020-12-05 18:31:38 +0100

    Added class MovieDto, should keep track of the state the movie has; rented, selected or rent which sets the text of the buttons. Removed the generic method; was getting ahead of myself too early.

--- RentalShopPoC-master/Store/MainWindow.xaml.cs
+++ RentalShopPoC-master/Store/MainWindow.xaml.cs
@@ -29,29 +29,53 @@ namespace Store
         private const int MOVIELISTSTARTY = 120;
 
 
+        //Knapp kan ha 3 lägen; uthyrd, hyra, vald.
+        public enum MovieSelection
+        {
+            Hyr, Vald, Rented
+        }
+        private class MovieDto
+        {
+            public MovieSelection Status { get; set; } = MovieSelection.Hyr;
+            public Movie TargetMovie { get; set; }
+            public MovieDto(Movie movie, Customer activeCustomer)
+            {
+                TargetMovie = movie;
 
-        private List<Movie> _chosenMovies = new List<Movie>();
+                if(_isRented(activeCustomer))
+                {
+                    Status = MovieSelection.Rented;
+                }
+            }
+            private bool _isRented(Customer activeCustomer)
+            {
+                return TargetMovie.Sales.Any(x => x.Customer.UserEmail.Equals(activeCustomer.UserEmail));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //StackPanel s = new StackPanel();
-
-            //var x = s.Children.Add(new Label();
-
             GreetText.Text = $"Välkommen {State.User.FirstName}";
 
-            State.Movies = API.GetMovieSlice(0, 40);
+            //State.Movies = API.GetMovieSlice(0, 40)
+
+            List<MovieDto> movies = new List<MovieDto>();
+
+            foreach(var m in API.GetMovieSlice(0,40))
+            {
+                movies.Add(new MovieDto(m, State.User));
+            }
 
             for (int y = 1; y < NUMOFROWS; y++)
             {
                 for (int x = 1; x <= NUMOFCOLS; x++)
                 {
                     int i = y * MovieGrid.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
+                    if (i < movies.Count)
                     {
-                        var movie = State.Movies[i];
-
+                        var movie = movies[i];
                         try
                         {
                             var stackPanel = new StackPanel()
@@ -59,22 +83,28 @@ namespace Store
                                 Orientation = Orientation.Vertical
                             };
 
-
                             //Define image properties
                             var image = new Image() {
                                 Cursor = Cursors.Hand,
                                 HorizontalAlignment = HorizontalAlignment.Center,
                                 VerticalAlignment = VerticalAlignment.Stretch,
-                                Source = new BitmapImage(new Uri(movie.ImageURL)),
+                                Source = new BitmapImage(new Uri(movie.TargetMovie.ImageURL)),
                                 Margin = new Thickness(4, 4, 4, 4)
                             };
                             image.Height = 140;
 
 
                             //Define button properties
-                            var button = new Button() { Content = "Hej" };
+
+                            //See if movie is already rented
+                            //if(_isRented(movie))
+                            //{
+
+                            //}
+                            var button = new Button();
+                            button.Content = movie.Status.ToString();
                             button.Width = 60;
-                            button.PreviewMouseUp += UIElementClicked<Button>;
+                            button.PreviewMouseUp += ButtonClicked;
 
 
                             stackPanel.Children.Add(image);
@@ -97,30 +127,47 @@ namespace Store
             }
         }
 
-
-        private void UIElementClicked<T>(object sender, MouseButtonEventArgs e) where T : FrameworkElement
+        private bool _isRented(Movie movie)
         {
-            if(sender is T)
+            var x = movie.Sales.Any(x => x.Customer.UserEmail.Equals(State.User.UserEmail));
+            return movie.Sales.Any(x => x.Customer.UserEmail.Equals(State.User.UserEmail));
+        }
+
+        private void _setMovieStatus(Movie movie)
+        {
+            
+        }
+
+        private void _rentMovies(Movie[] movies)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonClicked(object sender, MouseButtonEventArgs e) 
+        {
+            if(sender is Button)
             {
-                var stackPanel = (sender as T).Parent;
+
+                var stackPanel = (sender as Button).Parent;
 
                 var x = Grid.GetColumn(stackPanel as UIElement);
                 var y = Grid.GetRow(stackPanel as UIElement);
 
                 int i = y * MovieGrid.ColumnDefinitions.Count + x;
+
                 State.Pick = State.Movies[i];
 
-                if (API.RegisterSale(State.User, State.Pick))
-                    MessageBox.Show("All is well and you can download your movie now.", "Sale Succeeded!", MessageBoxButton.OK, MessageBoxImage.Information);
-                else
-                    MessageBox.Show("An error happened while buying the movie, please try again at a later time.", "Sale Failed!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                (sender as Button).Content = "Vald";
+
+
+
+                //if (API.RegisterSale(State.User, State.Pick))
+                //    MessageBox.Show("All is well and you can download your movie now.", "Sale Succeeded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                //else
+                //    MessageBox.Show("An error happened while buying the movie, please try again at a later time.", "Sale Failed!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
             }
-            
         }
 
-        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
+        
     }
 }
