84a7b291517f3e8f3591e943654bea28985ced43
Johannes Anttonen <johannesAnttonen@users.noreply.github.com>
2020-12-10 18:15:09 +0100

    Lates_migration_added

---
+++ RentalShopPoC-master/DatabaseConnection/Migrations/20201210165515_Lates_migration.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseConnection.Migrations
+{
+    public partial class Lates_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Movies_MovieId",
+                table: "Sales");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Customers",
+                newName: "LastName");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MovieId",
+                table: "Sales",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Movies_MovieId",
+                table: "Sales",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Movies_MovieId",
+                table: "Sales");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Customers",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MovieId",
+                table: "Sales",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Movies_MovieId",
+                table: "Sales",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
